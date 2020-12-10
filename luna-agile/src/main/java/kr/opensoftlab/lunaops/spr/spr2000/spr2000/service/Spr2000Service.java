b9115f44f0c926b6f1fca4c7bbf25b33e629d9a5
wistera4301 <wisteira4301@gmail.com>
2020-12-10 10:10:47 +0900

    feat : 스프린트 서비스 및 컨트롤러

--- luna-agile/src/main/java/kr/opensoftlab/lunaops/spr/spr2000/spr2000/service/Spr2000Service.java
+++ luna-agile/src/main/java/kr/opensoftlab/lunaops/spr/spr2000/spr2000/service/Spr2000Service.java
@@ -9,36 +9,36 @@ public interface Spr2000Service {
 
 	
 	@SuppressWarnings("rawtypes")
-	public int  selectSpr2000MmtListCnt(Map paramMap) throws Exception;
+	public int  selectSpr2000RptListCnt(Map paramMap) throws Exception;
 	
 	
 	@SuppressWarnings({ "rawtypes" })
-	public List<Map> selectSpr2000MmtList(Map paramMap) throws Exception;
+	public List<Map> selectSpr2000RptList(Map paramMap) throws Exception;
 	
 	
 	@SuppressWarnings({ "rawtypes" })
-	public Map  selectSpr2000MmtInfo(Map paramMap) throws Exception;
+	public Map  selectSpr2000RptInfo(Map paramMap) throws Exception;
 	
 	
 	@SuppressWarnings({ "rawtypes" })
-	public void  insertSpr2000MmtInfo(Map paramMap) throws Exception;
+	public void  insertSpr2000RptInfo(Map paramMap) throws Exception;
 	
 	
 	@SuppressWarnings({ "rawtypes"})
-	public void updateSpr2000MmtInfo(Map paramMap) throws Exception;
+	public void updateSpr2000RptInfo(Map paramMap) throws Exception;
 	
 	
 	@SuppressWarnings({ "rawtypes" })
-	public void deleteSpr2000MmtList(Map paramMap) throws Exception;
+	public void deleteSpr2000RptList(Map paramMap) throws Exception;
 
 	
 	@SuppressWarnings({ "rawtypes" })
-	public List<Map>  selectSpr2001MmtMemList(Map paramMap) throws Exception;
+	public List<Map>  selectSpr2001RptMemList(Map paramMap) throws Exception;
 	
 	@SuppressWarnings({ "rawtypes" })
-	public void insertSpr2001MmtMemList(Map paramMap) throws Exception;
+	public void insertSpr2001RptMemList(Map paramMap) throws Exception;
 	
 	
 	@SuppressWarnings({ "rawtypes" })
-	public void deleteSpr2001MmtMemList(Map paramMap) throws Exception;
+	public void deleteSpr2001RptMemList(Map paramMap) throws Exception;
 }
